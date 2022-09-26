300ed2690fd63145be73bde859138893489e4cb5
karmakat <rayanmoseskatoro@gmail.com>
2022-09-26 08:31:10 +0200

    changing

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Multipage Blog Website</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-</head>
-
-<body>
-    <nav>
-        <div class="container nav_container">
-            <a href="index.html" class="nav_logo">EGATOR</a>
-            <ul class="nav_items">
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.htm">Contact</a></li>
-                <!-- <li><a href="signin.html">Sign in</a></li> -->
-                <li class="nav_profile">
-                    <div class="avatar">
-                        <img src="img/avatar.jpg" alt="">
-                    </div>
-                    <ul>
-                        <li><a href="dashboard.html">Dashboard</a></li>
-                        <li><a href="logout.html">Log out</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <button id="open_nav-btn"><i class="icon-menu"></i></Menu></button>
-            <button id="close_nav-btn"><i class="icon-close"></i></button>
-        </div>
-    </nav>
+<?php include 'partials/header.php'?>
 
     <section class="featured">
         <div class="container featured_container">
