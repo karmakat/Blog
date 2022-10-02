98e958787c69c2f5b8f91d03f5e5b86aacab0574
karmakat <rayanmoseskatoro@gmail.com>
2022-10-02 10:58:46 +0200

    header avatar fixing

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,7 +1,5 @@
 <?php
-session_start();
-require 'config/database.php';
-require 'config/functions.php';
+require 'processing/header.process.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,10 +9,10 @@ require 'config/functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Multipage Blog Website</title>
-    <link rel="stylesheet" href="<?= ROOT_URL?>css/style.css">
-    <link rel="stylesheet" href="<?= ROOT_URL?>fonts/icomoon/style.css">
-    <script src="<?= ROOT_URL?>js/ajax.js"></script>
-    <script src="<?= ROOT_URL?>js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css">
+    <link rel="stylesheet" href="<?= ROOT_URL ?>fonts/icomoon/style.css">
+    <script src="<?= ROOT_URL ?>js/ajax.js"></script>
+    <script src="<?= ROOT_URL ?>js/jquery-3.3.1.min.js"></script>
 </head>
 
 <body>
@@ -22,19 +20,19 @@ require 'config/functions.php';
         <div class="container nav_container">
             <a href="http://localhost:4044" class="nav_logo">EGATOR</a>
             <ul class="nav_items">
-                <li><a href="<?= ROOT_URL?>home.php">Home</a></li>
-                <li><a href="<?= ROOT_URL?>blog.php">Blog</a></li>
-                <li><a href="<?= ROOT_URL?>about.php">About</a></li>
-                <li><a href="<?= ROOT_URL?>services.php">Services</a></li>
-                <li><a href="<?= ROOT_URL?>contact.php">Contact</a></li>
+                <li><a href="<?= ROOT_URL ?>home.php">Home</a></li>
+                <li><a href="<?= ROOT_URL ?>blog.php">Blog</a></li>
+                <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
+                <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
+                <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
                 <!-- <li><a href="signin.html">Sign in</a></li> -->
                 <li class="nav_profile">
                     <div class="avatar">
-                        <img src="<?= ROOT_URL?>img/avatar.jpg" alt="">
+                        <img src="<?= ROOT_URL ?>img/users_img/<?=$query_result->avatar?>" alt="">
                     </div>
                     <ul>
-                        <li><a href="<?= ROOT_URL?>admin/dashboard.php">Dashboard</a></li>
-                        <li><a href="<?= ROOT_URL?>signout.php">Log out</a></li>
+                        <li><a href="<?= ROOT_URL ?>admin/dashboard.php">Dashboard</a></li>
+                        <li><a href="<?= ROOT_URL ?>signout.php">Log out</a></li>
                     </ul>
                 </li>
             </ul>
---
+++ admin/processing/header.process.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require 'config/database.php';
+require 'config/functions.php';
+
+$select_all = "SELECT * FROM t_admins WHERE id = :id";
+$query_select = $db->prepare($select_all);
+$query_select->execute(['id' => $_SESSION['id']]);
+$query_result = $query_select->fetch(PDO::FETCH_OBJ);
+?>
