dbce7085f9625f20a23390741d6b5088c45e2001
karmakat <rayanmoseskatoro@gmail.com>
2022-10-02 08:30:53 +0200

    redirection with contact page

--- contact.php
+++ contact.php
@@ -1,10 +1,13 @@
-<?php include 'partials/header.php'?>
+<?php 
+require 'partials/header.php';
+require 'processing/contact.processing.php';
+?>
 
     <section class="empty_page">
         <h1>Contact Page</h1>
     </section>
 
-    <?php include 'partials/footer.php'?>
+    <?php require 'partials/footer.php'?>
 </body>
 
 </html>
