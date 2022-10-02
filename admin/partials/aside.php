e7c6de0d8dfb751d78eba8efb6e657669299af32
karmakat <rayanmoseskatoro@gmail.com>
2022-10-02 17:31:06 +0200

    the editor can not add a user:added

--- admin/partials/aside.php
+++ admin/partials/aside.php
@@ -10,17 +10,31 @@
                             <h5>Manage Posts</h5>
                         </a>
                     </li>
-                    
-                    <li>
+                    <?php
+                        $aside_ad_user = '
+                        <li>
                         <a href="add-user.php"><i class="icon-user-plus"></i>
                             <h5>Add Users</h5>
-                        </a>
-                    </li>
-                    <li>
+                         </a>
+                        </li>
+                        
+                        ';
+                        $aside_manage_user = '
+                        <li>
                         <a href="manage-users.php"><i class="icon-users"></i>
                             <h5>Manage Users</h5>
                         </a>
-                    </li>
+                        </li>
+                        ';
+                        if($_SESSION['level']==1){
+                            echo $aside_ad_user;
+                            echo $aside_manage_user;
+                        }else{
+                            
+                        }
+                    ?>
+                    
+                    
                     <li>
                         <a href="add-category.php"><i class="icon-th-list"></i>
                             <h5>Add Category</h5>
