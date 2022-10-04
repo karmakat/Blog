5dd57bc6313b5a633387fcad94bec4e7d0bf773d
karmakat <rayanmoseskatoro@gmail.com>
2022-10-04 20:19:14 +0200

    img problems

--- admin/processing/add-post.process.php
+++ admin/processing/add-post.process.php
@@ -28,30 +28,29 @@ if (isset($_POST['add'])) {
         if (empty($thumbmail)) {
             $errors[] = "Your thumbmail is required";
         } else {
-            $content_dir = "../../img/posts_img/";
+            $content_dir = "../img/";
 
             $tmp_file = $_FILES['thumbmail']['tmp_name'];
             if (!is_uploaded_file($tmp_file)) {
-                $errors[] = "File not found";
+                $errors [] = "File not found";
             }
             $type_file = $_FILES['thumbmail']['type'];
             if (!strstr($type_file, 'jpeg') && !strstr($type_file, 'png')) {
                 $errors[] = "This file is not an image";
             }
             $thumbmail_name = time() . '.jpg';
-            if (count($errors) == 0) {
-                if (!move_uploaded_file($tmp_file, $content_dir . $thumbmail_name)) {
-                    $errors[] = "Can not copy the file";
-                } else {
-                    $insert_query = "INSERT INTO t_posts(title,body,category,created_at,created_by,thumbmail)
-                    VALUES (?,?,?,now(),?,?)";
-                    $stmt = $db->prepare($insert_query);
-                    $stmt->execute([$title, $body, $category, $created_by, $thumbmail_name]);
+            if (!move_uploaded_file($tmp_file, $content_dir.$thumbmail_name)) {
+                $errors[] = "Can not copy the file";
+            }
+            if(count($errors) == 0){
+                $insert_query = "INSERT INTO t_posts(title,body,category,created_at,created_by,thumbmail)
+                VALUES (?,?,?,now(),?,?)";
+                $stmt = $db->prepare($insert_query);
+                $stmt->execute([$title,$body,$category,$created_by,$thumbmail_name]);
 
-                    set_flash($created_by . " your post was been addeed", "success");
-                    header('Location: views/dashboard.view.php?id=' . $_SESSION['id'] . 'username=' . $_SESSION['username'] . 'level=' . $_SESSION['level']);
-                    exit();
-                }
+                set_flash($created_by." your post was been addeed", "success");
+                header('Location: dashboard.php?id=' . $_SESSION['id'] . 'username=' . $_SESSION['username'].'level='.$_SESSION['level']);
+                exit();
             }
         }
     } else {
