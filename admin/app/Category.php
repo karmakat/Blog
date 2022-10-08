909db024c38684fc768210cfd6e6dc66a115a26b
karmakat <rayanmoseskatoro@gmail.com>
2022-10-08 09:05:53 +0200

    adding classes:category

--- admin/app/Category.php
+++ admin/app/Category.php
@@ -1,9 +1,16 @@
 <?php
 require '../../app/Publics.php';
-class Category{
+require '../../app/SignUp.php';
+class Category extends SignUp{
     public $dbcon = Publics::getPDO();
 
     // Add category
+    function insert_user($table,$elements){
+        $mysql =  "INSERT INTO t_categories(title,description,created_at,created_by)
+        VALUES(?,?,now(),?)";
+        $stmt = $this->dbcon->prepare($mysql);
+        $stmt->execute([$elements]);
+    }
     
 }
 ?>
